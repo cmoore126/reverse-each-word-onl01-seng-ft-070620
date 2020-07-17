f3a51d0a3257296dbe993f49889f6579db7c57c3
Clayton Moore <cmoorefour@gmail.com>
2020-07-17 18:18:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,6 @@ def reverse_each_word(sentence)
 array = [ ]
 binding.pry
 array << sentence.split(" ")
-array.collect {|sentence| word.join(" ").reverse}
+sentence.split(" ").collect {|sentence| word.join(" ").reverse}
 array.join(" ").reverse
 end
