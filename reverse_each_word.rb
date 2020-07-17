fa2a766004d9566c9ff43ce26fca2448a4b30657
Clayton Moore <cmoorefour@gmail.com>
2020-07-17 18:21:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,5 @@
-require 'pry'
+
 def reverse_each_word(sentence)
-array = [ ]
-binding.pry
-array << sentence.split(" ")
 array_2 = sentence.split(" ").collect {|word| word.reverse}
 array_2.join(" ")
 end
